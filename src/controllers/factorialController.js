8ea76d292f27a33d9561fde7db72cb91a2bdfc9d
Simmus7 <64326113+Simmus7@users.noreply.github.com>
2021-11-12 20:16:02 -0500

    creacion factorial

--- src/controllers/factorialController.js
+++ src/controllers/factorialController.js
@@ -1,7 +1,23 @@
 const factorialController = {};
 
 factorialController.factorial = (req, res) => {
-    res.json("hi")
+    let { n } = req.body;
+    if (isNaN(n) || n <= 0){
+        res.json("Por favor ingrese un número válido")
+    }
+    else {
+        var toReturn = {
+            n : factorial(n)
+        }
+        res.json(toReturn)
+    }
+}
+function factorial (n) {
+	var total = 1; 
+	for (i=1; i<=n; i++) {
+		total = total * i; 
+	}
+	return total; 
 }
 
 module.exports = factorialController;
