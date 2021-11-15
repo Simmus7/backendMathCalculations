bf7eda9d89944155bfb5e612af3d5b8caaa92106
Simmus7 <64326113+Simmus7@users.noreply.github.com>
2021-11-14 20:18:58 -0500

    mejorar resultado factorial

--- src/controllers/factorialController.js
+++ src/controllers/factorialController.js
@@ -2,12 +2,12 @@ const factorialController = {};
 
 factorialController.factorial = (req, res) => {
     let { n } = req.body;
-    if (isNaN(n) || n < 0 || n> 20000 || n%1 != 0){
+    if (isNaN(n) || n < 0 || n> 10000 || n%1 != 0){
     res.json("Unvalid numbers")
     }
     else {
         var toReturn = {
-            n : factorial(n).toString()
+            n : trim(factorial(n).toString())
         }
         res.json(toReturn)
     }
@@ -19,5 +19,22 @@ function factorial (n) {
     }
     return lut[n];
   };
+
+  function trim (cadena){
+      let newString = ""
+      let cont = 0;
+    for (let i=0; i<cadena.length; i++){
+        if(cont === 90){
+            cont = 0
+            newString = newString + cadena.charAt(i)+ " "
+        }
+        else {
+            newString = newString + cadena.charAt(i)
+            cont++
+
+        }
+    }
+    return newString
+  }
 
 module.exports = factorialController;
